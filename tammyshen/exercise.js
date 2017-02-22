d8984876ae8c07d9a0e506213c56018b11d26640
Yi-Tung Shen <yitung@mit.edu>
2017-02-22 10:57:32 -0500

    second test

---
+++ tammyshen/exercise.js
@@ -0,0 +1,38 @@
+var exercise = {};
+
+exercise.salaries = null;
+exercise.salary = null;
+
+// load salary and data
+exercise.load = function(salary, boston){
+    exercise.salaries = boston.data;
+    exercise.salary = salary;
+};
+
+// get salaries larger than given salary
+exercise.findBiggerSalaries = function(){
+
+    return exercise.salaries.filter (exercise.largerSalary);
+    // ---------------------------------------------------
+    //   YOUR CODE
+    //
+    //     Return a filtered list of salaries.
+    //     Return salaries larger than exercise.salary
+    //
+    //     Example: salaries larger than 300K is 8
+    // ---------------------------------------------------
+
+};
+
+// filter function
+exercise.largerSalary = function largerSalary(item){
+
+    return item [18] > exercise.salary;
+    // ---------------------------------------------------
+    //   YOUR CODE
+    //
+    //     Return a boolean (true/false) if item
+    //     is larger than exercise.salary
+    // ---------------------------------------------------
+
+};
